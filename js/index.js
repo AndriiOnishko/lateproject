940ba1d36c47f1d27bc932d939a559ec2f5d668b
onishkoandrii <or4ini@gmail.com>
2016-07-02 08:16:30 +0300

    next

---
+++ js/index.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+    "use strict";
+    PageAbilitiesHide();
+    PageContactHide();
+    PageAboutHide();
+    PageResumeHide();
+    PageProjectsHide();
+});
+function PageAboutShow() {
+    "use strict";
+    $("#page-contact").hide();
+    $("#page-abilities").hide();
+    $("#page-resume").hide();
+    $("#page-projects").hide();
+    $("#page-about").show();
+}
+function PageResumeShow() {
+    "use strict";
+    $("#page-contact").hide();
+    $("#page-abilities").hide();
+    $("#page-about").hide();
+    $("#page-projects").hide();
+    $("#page-resume").show();
+}
+function PageAbilitiesShow() {
+    "use strict";
+    $("#page-abilities").show();
+    $("#page-resume").hide();
+    $("#page-contact").hide();
+    $("#page-about").hide();
+    $("#page-projects").hide();
+}
+function PageProjectsShow() {
+    "use strict";
+    $("#page-contact").hide();
+    $("#page-abilities").hide();
+    $("#page-resume").hide();
+    $("#page-about").hide();
+    $("#page-projects").show();
+}
+function PageContactShow() {
+    "use strict";
+    $("#page-contact").show();
+    $("#page-resume").hide();
+    $("#page-abilities").hide();
+    $("#page-about").hide();
+}
+function PageAbilitiesHide() {
+    "use strict";
+    $("#page-abilities").hide();
+}
+function PageContactHide() {
+    "use strict";
+    $("#page-contact").hide();
+}
+function PageAboutHide() {
+    "use strict";
+    $("#page-about").hide();
+}
+function PageResumeHide() {
+    "use strict";
+    $("#page-resume").hide();
+}
+function PageProjectsHide() {
+    "use strict";
+    $("#page-projects").hide();
+}
+
